5575df3e799442f18078fda5c1142c22e556cf72
DANAWB29 <danawb29@gmail.com>
2025-10-22 16:49:59 +0300

    Updated job tracker frontend UI

--- eslint.config.js
+++ eslint.config.js
@@ -27,6 +27,3 @@ export default defineConfig([
     },
   },
 ])
-const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
-const city = "Addis Ababa";
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
